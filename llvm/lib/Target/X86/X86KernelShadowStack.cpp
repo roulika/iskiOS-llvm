050890e1262017610661dcb0523b1e77dbfce41c
Spyridoula Gravani <spyridoulika@gmail.com>
2020-08-04 10:48:16 -0400

    X86KernelShadowStack pass
    This patch implements a simple x86_64 kernel shadow call stack pass
    that uses PKK for protecting the integrity of the sensitive region.
    
    The patch adds the following instrumentations on every function
    prolog and epilog respectively:
    
    Prolog Instrumentation.
    1. Enable write access to shadow stack via wrpkru instruction
    2. Copy the return address on top of stack to the top of shadow stack
    3. Disable write access to shadow stack via wrpkru
    
    Epilog Instrumentation.
    1. Copy return address from shadow stack to stack

--- llvm/lib/Target/X86/X86KernelShadowStack.cpp
+++ llvm/lib/Target/X86/X86KernelShadowStack.cpp
@@ -19,7 +19,6 @@
 #include "X86InstrBuilder.h"
 #include "X86InstrInfo.h"
 #include "X86Subtarget.h"
-
 #include "llvm/CodeGen/MachineFunction.h"
 #include "llvm/CodeGen/MachineFunctionPass.h"
 #include "llvm/CodeGen/MachineInstrBuilder.h"
@@ -30,12 +29,14 @@
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
 
+#define PGSIZE 4096
+
 using namespace llvm;
 
 namespace {
 
 class X86KernelShadowStack : public MachineFunctionPass {
-public:
+ public:
   static char ID;
 
   X86KernelShadowStack() : MachineFunctionPass(ID) {
@@ -48,15 +49,115 @@ public:
 
   bool runOnMachineFunction(MachineFunction &Fn) override;
 
-private:
+ private:
 };
 
 char X86KernelShadowStack::ID = 0;
 
-} // end anonymous namespace.
+}  // end anonymous namespace.
 
 bool X86KernelShadowStack::runOnMachineFunction(MachineFunction &Fn) {
-  return false;
+  /* Parts of kernel boot loader (i.e., /arch/x86/boot/compressed/*) */
+  if ((Fn.getTarget().getCodeModel() != CodeModel::Kernel)) {
+    errs() << "CodeModel is not Kernel "
+           << Fn.getMMI().getModule()->getSourceFileName() << "\n";
+    return false;
+  }
+
+  if (Fn.getFunction().hasFnAttribute(Attribute::Naked)) return false;
+
+  if (Fn.empty()) return false;
+
+  if (!Fn.getRegInfo().tracksLiveness()) {
+    errs() << "function doesn't track liveness, skipping...\n";
+    return false;
+  }
+
+  if (!Fn.getSubtarget<X86Subtarget>().is64Bit()) return false;
+
+  if (Fn.getName() == "sync_regs" ||
+      Fn.getName() == "prepare_exit_to_usermode" ||
+      Fn.getName() == "__startup_secondary_64")
+    return false;
+
+  bool HasReturn = false;
+  for (auto &MBB : Fn) {
+    if (MBB.empty()) continue;
+
+    const MachineInstr &MI = MBB.instr_back();
+    if (MI.isReturn()) HasReturn = true;
+  }
+
+  if (!HasReturn) return false;
+
+  MachineBasicBlock &MBB = Fn.front();
+  const MachineBasicBlock *NonEmpty = MBB.empty() ? MBB.getFallThrough() : &MBB;
+  const DebugLoc &DL = NonEmpty->front().getDebugLoc();
+  const TargetInstrInfo *TII = Fn.getSubtarget().getInstrInfo();
+
+  auto MBBI = MBB.begin();
+
+  /* Prolog Instrumentation */
+
+  // push rcx
+  BuildMI(MBB, MBBI, DL, TII->get(X86::PUSH64r))
+      .addReg(X86::RCX, RegState::Kill);
+
+  // push rdx
+  BuildMI(MBB, MBBI, DL, TII->get(X86::PUSH64r))
+      .addReg(X86::RDX, RegState::Kill);
+
+  // xor rcx, rcx
+  BuildMI(MBB, MBBI, DL, TII->get(X86::XOR64rr))
+      .addDef(X86::RCX)
+      .addReg(X86::RCX, RegState::Undef)
+      .addReg(X86::RCX, RegState::Undef);
+
+  // rdpkru
+  BuildMI(MBB, MBBI, DL, TII->get(X86::RDPKRUr));
+
+  // wrpkru
+  BuildMI(MBB, MBBI, DL, TII->get(X86::WRPKRUr));
+
+  // mov r10, [rsp+0x10]
+  addRegOffset(BuildMI(MBB, MBBI, DL, TII->get(X86::MOV64rm)).addDef(X86::R10),
+               X86::RSP, false, 0x10);
+
+  // mov [rsp+0x10-4*PGSIZE], r10
+  addRegOffset(BuildMI(MBB, MBBI, DL, TII->get(X86::MOV64mr)), X86::RSP, false,
+               0x10 - 4 * PGSIZE)
+      .addDef(X86::R10);
+
+  // wrpkru
+  BuildMI(MBB, MBBI, DL, TII->get(X86::WRPKRUr));
+
+  // pop rdx
+  BuildMI(MBB, MBBI, DL, TII->get(X86::POP64r))
+      .addReg(X86::RDX, RegState::Kill);
+
+  // pop rcx
+  BuildMI(MBB, MBBI, DL, TII->get(X86::POP64r))
+      .addReg(X86::RCX, RegState::Kill);
+
+  /* Epilog Instrumentation */
+
+  for (auto &MBB : Fn) {
+    if (MBB.empty()) continue;
+
+    MachineInstr &MI = MBB.instr_back();
+    if (MI.isReturn()) {
+      // mov r10, [rsp-4*PGSIZE]
+      addRegOffset(BuildMI(MBB, MI, MI.getDebugLoc(), TII->get(X86::MOV64rm))
+                       .addDef(X86::R10),
+                   X86::RSP, false, -4 * PGSIZE);
+
+      // mov [rsp], r10
+      addDirectMem(BuildMI(MBB, MI, MI.getDebugLoc(), TII->get(X86::MOV64mr)),
+                   X86::RSP)
+          .addReg(X86::R10);
+    }
+  }
+  return true;
 }
 
 INITIALIZE_PASS(X86KernelShadowStack, "kernel-shadow-stack",
