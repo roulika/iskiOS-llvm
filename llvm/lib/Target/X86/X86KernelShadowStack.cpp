5c936acf79fe7b888461f127eae4fc85e694fabf
Spyridoula Gravani <spyridoulika@gmail.com>
2020-08-10 21:07:09 -0400

    Race-free Shadow Stack Patch
    The commit instruments all callsites as well as the prolog
    and epilog of every function in a module. In IskiOS's shadow
    stack, we maintain the following invariant: on entry to a function,
    %r10 contains the return address. On function return, %r10 contains
    the return address and we jump through %r10.
    Since the return address is passed through a register,
    there's no opportunity for race between CALL and function entry.
    In addition, since we jump through %r10, there's no opportunity
    for race between ret instruction and actual return.
    
    Function Prolog Instrumentation:
    
    The added instructions compute the address of the shadow stack,
    enable write access to shadow stack, store the address
    saved in %r10 (i.e., the return address) to the top of the
    shadow stack, and disable the write access.
    
    Function Epilog Instrumentation:
    
    The added instructions copy the return address from the top
    of the shadow stack to %r10 and jump to that return address.
    
    Callsite Instrumentation:
    
    Before the CALL instruction, the pass adds a LEA instruction
    which loads the effective address of the return site to %r10.
    
    Note that this shadow stack needs CONFIG_ISKIOS_SHADOW_STACK to
    be enabled in the IskiOS kernel.

--- llvm/lib/Target/X86/X86KernelShadowStack.cpp
+++ llvm/lib/Target/X86/X86KernelShadowStack.cpp
@@ -9,10 +9,23 @@
 // All Right Reserved.
 //
 //===---------------------------------------------------------------------===//
-// The X86KernelShadowStack pass instruments function prologs/epilogs to check
-// that the return address has not been corrupted during the execution of the
-// function. The return address is stored in a 'shadow call stack' addressed
-// by flipping the 15th bit of the current %rsp.
+// The X86KernelShadowStack pass implements a race-free shadow stack for the
+// Linux kernel on x86_64. In particular, it instruments function prologs with
+// code that enables access to a write-protected shadow call stack, stores the
+// address on top of the shadow stack and disables write access using the
+// wrpkru instruction.
+//
+// The pass also instruments every function epilog with code that moves the
+// return address from the shadow stack to %r10 and then jumps to that
+// address. Replacing the return instruction with a jump prevents race hazards
+// inherent to the x86_64 ret implementation (pop+jmp microarchitecturally).
+//
+// Finally, the pass instruments callsites with code that passes the return
+// address to the callee via %r10 in order to prevent race hazards inherent to
+// the x86_64 "call" implementation (push+jmp microarchitecturally).
+
+// Note: the shadow call stack is addressed 0x4000 relative current %rsp since
+// we've increased each stack in the kernel by 4KB pages.
 //===---------------------------------------------------------------------===//
 
 #include "X86.h"
@@ -75,69 +88,94 @@ bool X86KernelShadowStack::runOnMachineFunction(MachineFunction &Fn) {
 
   if (!Fn.getSubtarget<X86Subtarget>().is64Bit()) return false;
 
-  if (Fn.getName() == "sync_regs" ||
-      Fn.getName() == "prepare_exit_to_usermode" ||
-      Fn.getName() == "__startup_secondary_64")
+  if (Fn.getName() == "sync_regs")
     return false;
-
-  bool HasReturn = false;
-  for (auto &MBB : Fn) {
-    if (MBB.empty()) continue;
-
-    const MachineInstr &MI = MBB.instr_back();
-    if (MI.isReturn()) HasReturn = true;
-  }
-
-  if (!HasReturn) return false;
 
   MachineBasicBlock &MBB = Fn.front();
   const MachineBasicBlock *NonEmpty = MBB.empty() ? MBB.getFallThrough() : &MBB;
   const DebugLoc &DL = NonEmpty->front().getDebugLoc();
   const TargetInstrInfo *TII = Fn.getSubtarget().getInstrInfo();
 
-  auto MBBI = MBB.begin();
+  if (Fn.getName() != "copy_user_handle_tail" &&
+      Fn.getName() != "__startup_64" &&
+      Fn.getName() != "prepare_exit_to_usermode" &&
+      Fn.getName() != "__startup_secondary_64") {
+    auto MBBI = MBB.begin();
 
-  /* Prolog Instrumentation */
+    /* Prolog Instrumentation */
 
-  // push rcx
-  BuildMI(MBB, MBBI, DL, TII->get(X86::PUSH64r))
-      .addReg(X86::RCX, RegState::Kill);
+    // push rcx
+    BuildMI(MBB, MBBI, DL, TII->get(X86::PUSH64r))
+        .addReg(X86::RCX, RegState::Kill);
 
-  // push rdx
-  BuildMI(MBB, MBBI, DL, TII->get(X86::PUSH64r))
-      .addReg(X86::RDX, RegState::Kill);
+    // push rdx
+    BuildMI(MBB, MBBI, DL, TII->get(X86::PUSH64r))
+        .addReg(X86::RDX, RegState::Kill);
 
-  // xor rcx, rcx
-  BuildMI(MBB, MBBI, DL, TII->get(X86::XOR64rr))
-      .addDef(X86::RCX)
-      .addReg(X86::RCX, RegState::Undef)
-      .addReg(X86::RCX, RegState::Undef);
+    // xor rcx, rcx
+    BuildMI(MBB, MBBI, DL, TII->get(X86::XOR64rr))
+        .addDef(X86::RCX)
+        .addReg(X86::RCX, RegState::Undef)
+        .addReg(X86::RCX, RegState::Undef);
 
-  // rdpkru
-  BuildMI(MBB, MBBI, DL, TII->get(X86::RDPKRUr));
+    // rdpkru
+    BuildMI(MBB, MBBI, DL, TII->get(X86::RDPKRUr));
 
-  // wrpkru
-  BuildMI(MBB, MBBI, DL, TII->get(X86::WRPKRUr));
+    // xor %rax, $(1 << 20)
+    BuildMI(MBB, MBBI, DL, TII->get(X86::XOR64ri32), X86::RAX)
+        .addReg(X86::RAX)
+        .addImm(1 << 20);
 
-  // mov r10, [rsp+0x10]
-  addRegOffset(BuildMI(MBB, MBBI, DL, TII->get(X86::MOV64rm)).addDef(X86::R10),
-               X86::RSP, false, 0x10);
+    // wrpkru
+    BuildMI(MBB, MBBI, DL, TII->get(X86::WRPKRUr));
 
-  // mov [rsp+0x10-4*PGSIZE], r10
-  addRegOffset(BuildMI(MBB, MBBI, DL, TII->get(X86::MOV64mr)), X86::RSP, false,
-               0x10 - 4 * PGSIZE)
-      .addDef(X86::R10);
+    // mov [rsp+0x10-4*PGSIZE], r10
+    addRegOffset(BuildMI(MBB, MBBI, DL, TII->get(X86::MOV64mr)), X86::RSP,
+                 false, 0x10 - 4 * PGSIZE)
+        .addDef(X86::R10);
 
-  // wrpkru
-  BuildMI(MBB, MBBI, DL, TII->get(X86::WRPKRUr));
+    // xor %rax, $(1 << 20)
+    BuildMI(MBB, MBBI, DL, TII->get(X86::XOR64ri32), X86::RAX)
+        .addReg(X86::RAX)
+        .addImm(1 << 20);
 
-  // pop rdx
-  BuildMI(MBB, MBBI, DL, TII->get(X86::POP64r))
-      .addReg(X86::RDX, RegState::Kill);
+    // wrpkru
+    BuildMI(MBB, MBBI, DL, TII->get(X86::WRPKRUr));
 
-  // pop rcx
-  BuildMI(MBB, MBBI, DL, TII->get(X86::POP64r))
-      .addReg(X86::RCX, RegState::Kill);
+    // pop rdx
+    BuildMI(MBB, MBBI, DL, TII->get(X86::POP64r))
+        .addReg(X86::RDX, RegState::Kill);
+
+    // pop rcx
+    BuildMI(MBB, MBBI, DL, TII->get(X86::POP64r))
+        .addReg(X86::RCX, RegState::Kill);
+
+#if 0
+  /* NOTE: Uncomment the following two instructions to identify
+   * uninstrumented functions -- part of *.S files that LLVM does
+   * not go over. It will cause infinite-loop which you can use GDB
+   * to find the culprit callsite and address the issue. Good Luck!
+   */
+  {
+    auto MBBI = MBB.begin();
+    // r10 == [rsp]? If not, spin till we find you!
+
+    MCSymbol *LoopSymbol = Fn.getContext().createTempSymbol();
+
+    // cmp r10, [rsp]
+    auto cmpInst = addDirectMem(
+        BuildMI(MBB, MBBI, DL, TII->get(X86::CMP64rm)).addDef(X86::R10),
+        X86::RSP);
+
+    cmpInst->setPreInstrSymbol(Fn, LoopSymbol);
+
+    // jne LoopSymbol
+    BuildMI(MBB, MBBI, DL, TII->get(X86::JCC_1))
+        .addSym(LoopSymbol)
+        .addImm(X86::COND_NE);
+  }
+#endif
+  }
 
   /* Epilog Instrumentation */
 
@@ -146,15 +184,83 @@ bool X86KernelShadowStack::runOnMachineFunction(MachineFunction &Fn) {
 
     MachineInstr &MI = MBB.instr_back();
     if (MI.isReturn()) {
-      // mov r10, [rsp-4*PGSIZE]
-      addRegOffset(BuildMI(MBB, MI, MI.getDebugLoc(), TII->get(X86::MOV64rm))
-                       .addDef(X86::R10),
-                   X86::RSP, false, -4 * PGSIZE);
+      if (MI.isCall()) continue;
 
-      // mov [rsp], r10
-      addDirectMem(BuildMI(MBB, MI, MI.getDebugLoc(), TII->get(X86::MOV64mr)),
-                   X86::RSP)
-          .addReg(X86::R10);
+      if (Fn.getName() != "copy_user_handle_tail" &&
+          Fn.getName() != "__startup_64" &&
+          Fn.getName() != "prepare_exit_to_usermode" &&
+          Fn.getName() != "__startup_secondary_64") {
+        /* Use jmp to avoid race. */
+
+        // mov r10, [rsp-4*PGSIZE]
+        addRegOffset(BuildMI(MBB, MI, MI.getDebugLoc(), TII->get(X86::MOV64rm))
+                         .addDef(X86::R10),
+                     X86::RSP, false, -4 * PGSIZE);
+
+        // add rsp, 0x8
+        BuildMI(MBB, MI, MI.getDebugLoc(), TII->get(X86::ADD64ri8), X86::RSP)
+            .addReg(X86::RSP)
+            .addImm(0x8);
+
+        // jmp *r10
+        BuildMI(MBB, MI, MI.getDebugLoc(), TII->get(X86::JMP64r))
+            .addReg(X86::R10);
+
+        MI.eraseFromParent();
+      }
+    }
+  }
+
+  /* Callsite Instrumentation */
+
+  for (auto &MBB : Fn) {
+    if (MBB.empty()) continue;
+    for (auto &MI : MBB) {
+      if (MI.isCall()) {
+#if 1
+        if (MI.isReturn()) {
+          // mov r10, [rsp-4*PGSIZE]
+          addRegOffset(
+              BuildMI(MBB, MI, MI.getDebugLoc(), TII->get(X86::MOV64rm))
+                  .addDef(X86::R10),
+              X86::RSP, false, -4 * PGSIZE);
+
+          continue;
+        }
+
+        MCSymbol *RetSymbol = Fn.getContext().createTempSymbol();
+        MI.setPostInstrSymbol(Fn, RetSymbol);
+
+        /* Note: If the call is already using R10 as a pointer, we change it
+         * to use R11 instead.
+         */
+        if (MI.findRegisterUseOperand(X86::R10) ||
+            MI.hasRegisterImplicitUseOperand(X86::R10)) {
+          // mov r11, r10
+          BuildMI(MBB, MI, MI.getDebugLoc(), TII->get(X86::MOV64rr), X86::R11)
+              .addReg(X86::R10);
+
+          // lea r10, RetSymbol
+          BuildMI(MBB, MI, MI.getDebugLoc(), TII->get(X86::LEA64r), X86::R10)
+              .addReg(/*Base*/ X86::RIP)
+              .addImm(/*Scale*/ 1)
+              .addReg(/*Index*/ 0)
+              .addSym(RetSymbol)
+              .addReg(/*Segment*/ 0);
+
+          MI.getOperand(0).setReg(X86::R11);
+        } else {
+          // lea r10, RetSymbol
+          BuildMI(MBB, MI, MI.getDebugLoc(), TII->get(X86::LEA64r), X86::R10)
+              .addReg(/*Base*/ X86::RIP)
+              .addImm(/*Scale*/ 1)
+              .addReg(/*Index*/ 0)
+              .addSym(RetSymbol)
+              .addReg(/*Segment*/ 0);
+        }
+#endif
+        // callq
+      }
     }
   }
   return true;
